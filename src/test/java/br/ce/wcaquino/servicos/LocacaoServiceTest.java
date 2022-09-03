2d1b2600325f9070903b7c2634a3c9d74bbfdcbb
MarioLuiz <marioluiz.arruda@gmail.com>
2022-09-02 21:46:20 -0400

    Implementando assertThat

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,9 +1,13 @@
 package br.ce.wcaquino.servicos;
 
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Date;
 
-import org.junit.Assert;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import br.ce.wcaquino.entidades.Filme;
@@ -25,11 +29,16 @@ public class LocacaoServiceTest {
 		Locacao locacao = service.alugarFilme(usuario,filme);
 		
 		// verificacao
-		Assert.assertTrue(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()));
+		assertTrue(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()));
+		assertThat(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()), CoreMatchers.is(true));
 		
-		Assert.assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()));
+		assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()));
+		assertThat(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()), CoreMatchers.is(true));
 		
-		Assert.assertEquals(10.00 , locacao.getValor(), 0.01);
+		assertEquals(10.00 , locacao.getValor(), 0.01);
+		assertThat(locacao.getValor(), CoreMatchers.is(10.00));
+		assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(10.00)));
+		assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(6.00)));
 		
 	}
 }
