9f544e870de3ffbc3843d622f30318ce1782e19e
MarioLuiz <marioluiz.arruda@gmail.com>
2022-09-04 23:11:32 -0400

    Adicionando exception e pegando com assert.fail

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.Date;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -16,6 +17,7 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
+
 
 public class LocacaoServiceTest {
 	
@@ -28,27 +30,40 @@ public class LocacaoServiceTest {
 		// cenario
 		LocacaoService service = new  LocacaoService();
 		Usuario usuario =  new Usuario("Mario");
-		Filme filme = new Filme("Uma linda mulher", 1, 10.00);
+		Filme filme = new Filme("Uma linda mulher", 0, 10.00);
 		
 		// acao
-		Locacao locacao = service.alugarFilme(usuario,filme);
+		Locacao locacao = null;
+		try {
+			locacao = service.alugarFilme(usuario,filme);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		// verificacao
-		assertTrue(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()));
-		assertThat(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()), CoreMatchers.is(true));
+		try {
+			// verificacao
+			assertTrue(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()));
+			assertThat(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()), CoreMatchers.is(true));
+			
+			assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()));
+			assertThat(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()), CoreMatchers.is(true));
+			
+			assertEquals(10.00 , locacao.getValor(), 0.01);
+			assertThat(locacao.getValor(), CoreMatchers.is(10.00));
+			assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(10.00)));
+			assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(6.00)));
+			
+			// verificacao com errorCollector
+			error.checkThat(locacao.getValor(), CoreMatchers.is(10.00));
+			error.checkThat(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()), CoreMatchers.is(true));
+			error.checkThat(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()), CoreMatchers.is(true));
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail("Não deveria lançar exception");
+		}
 		
-		assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()));
-		assertThat(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()), CoreMatchers.is(true));
 		
-		assertEquals(10.00 , locacao.getValor(), 0.01);
-		assertThat(locacao.getValor(), CoreMatchers.is(10.00));
-		assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(10.00)));
-		assertThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.not(6.00)));
-		
-		// verificacao com errorCollector
-		error.checkThat(locacao.getValor(), CoreMatchers.is(10.00));
-		error.checkThat(DataUtils.isMesmaData(new Date(),locacao.getDataLocacao()), CoreMatchers.is(true));
-		error.checkThat(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1),locacao.getDataRetorno()), CoreMatchers.is(true));
 		
 	}
 }
