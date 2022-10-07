ec24ea797d1df099e955ad12dd170e2c10092ac3
MarioLuiz <marioluiz.arruda@gmail.com>
2022-10-06 21:48:38 -0400

    Entendendo difereça entre Spy e Mock

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,10 +1,36 @@
 package br.ce.wcaquino.servicos;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class CalculadoraMockTest {
+	
+	@Mock
+	private Calculadora calcMock;
+	
+	// Nao funciona com interfaces, apenas classes concretas
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+	}
+	
+	@Test
+	public void deveMostrarDiferencaEntreMockSpy() {
+		Mockito.when(calcMock.somar(1, 2)).thenReturn(5);
+		Mockito.when(calcSpy.somar(1, 2)).thenReturn(5);
+		
+		System.out.println("Mock: " + calcMock.somar(1, 3));
+		System.out.println("Spy: " + calcSpy.somar(1, 3));
+		
+	}
 	
 	@Test
 	public void teste() {
@@ -13,7 +39,7 @@ public class CalculadoraMockTest {
 		ArgumentCaptor<Integer> argCapt = ArgumentCaptor.forClass(Integer.class);
 		Mockito.when(calc.somar(argCapt.capture(), argCapt.capture())).thenReturn(5);
 		
-		System.out.println(calc.somar(1010, 8080));
-		System.out.println(argCapt.getAllValues());
+		//System.out.println(calc.somar(1010, 8080));
+		//System.out.println(argCapt.getAllValues());
 	}
 }
